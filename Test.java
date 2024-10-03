5cb2f626c793c87b689731afd3ffb1e07b23f3b7
nsgram <68147300+nsgram@users.noreply.github.com>
2024-10-03 14:02:03 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,13 +1,11 @@
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
+import com.azure.core.util.FluxUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-import reactor.core.publisher.Flux;
 
 @Service
 public class AzureBlobService {
@@ -25,20 +23,13 @@ public class AzureBlobService {
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
         BlobClient blobClient = containerClient.getBlobClient(fileName);
 
-        // Use the reactive dataBuffer stream from the FilePart object and transfer it to the blob output stream
-        return filePart.content()
-                .flatMap(dataBuffer -> {
-                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
-                    dataBuffer.read(bytes);
-                    dataBuffer.release();
-                    byteArrayOutputStream.write(bytes, 0, bytes.length);
-
-                    // Now upload this chunk to Azure Blob
-                    return Mono.fromCallable(() -> {
-                        blobClient.getBlockBlobClient().upload(BinaryData.fromBytes(byteArrayOutputStream.toByteArray()), true);
-                        return true;
-                    }).then();
-                }).then();
+        return blobClient.getBlockBlobAsyncClient()
+            .upload(FluxUtil.toFluxByteBuffer(filePart.content()), null, true)
+            .then();
     }
 }
+
+
+BlobHttpHeaders headers = new BlobHttpHeaders()
+    .setContentType(filePart.headers().getContentType().toString());
+blobClient.setHttpHeaders(headers);
