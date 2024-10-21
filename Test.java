492f0fe8cf781d1cff676e49ae4bae9b133bd52a
nsgram <68147300+nsgram@users.noreply.github.com>
2024-10-21 13:20:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,2 +1,4 @@
-logging.level.org.springframework.web=DEBUG
-logging.level.org.springframework.web.reactive.function.client=DEBUG
+# Set WebFlux timeouts for reading, writing, and connection
+spring.webflux.timeout.read=5000    # Timeout for reading request body in milliseconds
+spring.webflux.timeout.write=5000   # Timeout for writing response body in milliseconds
+spring.webflux.timeout.connection=5000  # Connection timeout in milliseconds
