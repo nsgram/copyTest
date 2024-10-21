0315a9deb4938c01b02e9e4af78428271b0d36d7
nsgram <68147300+nsgram@users.noreply.github.com>
2024-10-21 16:32:32 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,2 @@
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-@Configuration
-@EnableWebFlux
-public class WebConfig implements WebFluxConfigurer {
-
-    @Override
-    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
-        configurer.defaultCodecs().maxInMemorySize(-1);
-    }
-}
+spring.webflux.multipart.max-file-size=10MB
+spring.webflux.multipart.max-request-size=10MB
