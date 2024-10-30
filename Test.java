b4235debd97f488234841121c117b86877f29b72
nsgram <68147300+nsgram@users.noreply.github.com>
2024-10-30 20:39:06 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,18 +1,14 @@
-@Test
-void testGetAllGroups() throws Exception {
-    String keyString = "{\"performerId\": \"2993443\", \"performerAgencyId\": \"288888\", \"performerType\": \"EXTERNAL\", \"performerRole\": \"BROKER\", \"performerPrivilege\": \"SHARED\"}";
-
-    ObjectMapper objMapper = new ObjectMapper();
-
-    // Parse keyString to KeyIdentifiers to verify the structure
-    KeyIdentifiers keyIdentifiers = objMapper.readValue(keyString, KeyIdentifiers.class);
-
-    // Mock ObjectMapper's behavior to return a KeyIdentifiers object
-    when(objMapper.readValue(anyString(), KeyIdentifiers.class)).thenReturn(keyIdentifiers);
-
-    // Invoke the controller method
-    quotesController.getRecentQuotesForEachGroup(qyoteType, keyString);
-
-    // Verify the service method call
-    then(quotesService).should(atLeastOnce()).getRecentQuotesForEachGroup(qyoteType, performerTypeSuperUser, keyIdentifiers);
-}
+Argument(s) are different! Wanted:
+quotesService.getRecentQuotesForEachGroup(
+    "ILLUS",
+    "SUPERUSER",
+    KeyIdentifiers(performerId=2993443, performerAgencyId=288888, performerType=EXTERNAL, performerRole=SUPERUSER, performerPrivilege=SHARED)
+);
+-> at com.aetna.asgwy.controller.QuotesControllerTest.testGetAllGroups(QuotesControllerTest.java:70)
+Actual invocations have different arguments at position [2]:
+quotesService.getRecentQuotesForEachGroup(
+    "ILLUS",
+    "SUPERUSER",
+    KeyIdentifiers(performerId=2993443, performerAgencyId=288888, performerType=EXTERNAL, performerRole=SUPERUSER, performerPrivilege=SHARED)
+);
+-> at com.aetna.asgwy.controller.QuotesController.getRecentQuotesForEachGroup(QuotesController.java:59)
