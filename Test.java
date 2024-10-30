3ea644807be3571b9a0bf366b763643c7721b855
nsgram <68147300+nsgram@users.noreply.github.com>
2024-10-30 20:51:47 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,1 @@
-Argument(s) are different! Wanted:
-quotesService.getRecentQuotesForEachGroup(
-    "ILLUS",
-    "SUPERUSER",
-    KeyIdentifiers(performerId=2993443, performerAgencyId=288888, performerType=EXTERNAL, performerRole=SUPERUSER, performerPrivilege=SHARED)
-);
--> at com.aetna.asgwy.controller.QuotesControllerTest.testGetAllGroups(QuotesControllerTest.java:70)
-Actual invocations have different arguments at position [2]:
-quotesService.getRecentQuotesForEachGroup(
-    "ILLUS",
-    "SUPERUSER",
-    KeyIdentifiers(performerId=2993443, performerAgencyId=288888, performerType=EXTERNAL, performerRole=SUPERUSER, performerPrivilege=SHARED)
-);
--> at com.aetna.asgwy.controller.QuotesController.getRecentQuotesForEachGroup(QuotesController.java:59)
+1
