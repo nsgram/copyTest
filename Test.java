e48a873cdabf8b64776b9743a6a40cc525d857e9
nsgram <68147300+nsgram@users.noreply.github.com>
2024-11-12 11:28:51 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,13 @@
-if (StringUtils.isNotBlank(quoteType)) {
-				if (!BROKER.equalsIgnoreCase(keyIdentifiers.getUserrole())) {
+groupDTOList = entityManager.createNativeQuery(latestQuotesByType.getQuery(), "GetQuotes")
+    .setParameter(1, quoteType)
+    .setFirstResult(startIndex)
+    .setMaxResults(page.getPageSize())
+    .unwrap(org.hibernate.query.NativeQuery.class)
+    .addEntity(DashboardDTO.class) // Map result to DashboardDTO
+    .getResultList();
 
-					LatestQuotesByType latestQuotesByType = new LatestQuotesByType(dashBoardRequest,sortInput);
-					groupDTOList = entityManager.createNativeQuery(latestQuotesByType.getQuery(), "GetQuotes")
-							.setParameter(1, quoteType).setFirstResult(startIndex).setMaxResults(page.getPageSize()).getResultList();
-				}
-
-//Type safety: The expression of type List needs unchecked conversion to conform to List<DashboardDTO>
+TypedQuery<DashboardDTO> query = entityManager.createQuery("SELECT new com.example.DashboardDTO(...) FROM YourEntity e WHERE ...", DashboardDTO.class);
+groupDTOList = query.setParameter(1, quoteType)
+                    .setFirstResult(startIndex)
+                    .setMaxResults(page.getPageSize())
+                    .getResultList();
