21e0200196528160b2e9798bdb2c7fc5758f1b7e
nsgram <68147300+nsgram@users.noreply.github.com>
2024-11-19 11:30:49 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,53 @@
-import java.util.UUID;
+public class AVScanFileDecryption {
 
-public class RandomStringGenerator {
-    public static void main(String[] args) {
-        String randomString = generateRandomString();
-        System.out.println(randomString);
-    }
+	public static void main(String[] args) throws Exception {
+		// Inputs
+		String jwe = "QQ";
 
-    public static String generateRandomString() {
-        double random = Math.random() + 1;
-        String base36String = Double.toString(random, 36);
-        return base36String.substring(base36String.indexOf('.') + 1);
-    }
+		PrivateKey privateKey = AVScanFileEncrtption.readPrivateKey();
+
+		// Decrypt JWE Token
+		JWEObject jweObject = JWEObject.parse(jwe);
+		jweObject.decrypt(new RSADecrypter(privateKey));
+		String plaintext = jweObject.getPayload().toString();
+
+		// Parse JWT
+		SignedJWT signedJWT = SignedJWT.parse(plaintext);
+		JWTClaimsSet claimsSet = signedJWT.getJWTClaimsSet();
+		System.out.println("Payload: " + claimsSet.toJSONObject());
+
+		// Generate Download Token
+		Map<String, Object> header = getHeader();
+		Map<String, Object> payload = new HashMap<>();
+		payload.put("cvs_av_file_ref", claimsSet.getStringClaim("cvs_av_file_ref"));
+		payload.put("x-lob", "security-engineering");
+		payload.put("scope", "openid email");
+		// payload.put("jti", UUID.randomUUID().toString());
+		payload.put("jti", Long.toString((long) ((Math.random() + 1) * 1_000_000), 36).substring(2));
+		payload.put("aud", "CVS-AVScan");
+		payload.put("iss", "Aetna-Sales-Gateway");
+		payload.put("sub", "download_bearer_token");
+
+		String token = createJwt(payload, header, privateKey);
+		System.out.println("Generated Download Token: " + token);
+	}
+
+	private static Map<String, Object> getHeader() {
+		Map<String, Object> header = new HashMap<>();
+		header.put("alg", "RS256");
+		header.put("typ", "JWT");
+		header.put("kid", "dd");
+		header.put("expiresIn", new Date(System.currentTimeMillis() + 3600 * 1000));
+
+		return header;
+	}
+
+	public static String createJwt(Map<String, Object> payload, Map<String, Object> header, PrivateKey privateKey)
+			throws Exception {
+		JWSHeader jwsHeader = new JWSHeader.Builder(JWSHeader.parse(header)).build();
+		JWSObject jwsObject = new JWSObject(jwsHeader, new com.nimbusds.jose.Payload(payload));
+
+		jwsObject.sign(new RSASSASigner(privateKey));
+		return jwsObject.serialize();
+	}
 }
