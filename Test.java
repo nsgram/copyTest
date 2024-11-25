39334e395861534385373578e93dd5b2308e6d85
nsgram <68147300+nsgram@users.noreply.github.com>
2024-11-25 16:08:22 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,3 @@
-curl --location --request GET 'https://sit1-api.cvshealth.com/file/scan/download/v1/files/1732518835813-screenshot-6bb313e5' \
---header 'x-api-key: T1gpDfjoNoNPdqqVfGgR1kw3Rnz0oi6w' \
---header 'Authorization: Bearer eyJlxY14RiFJ0vd6bXq17dR-fBDldRb1g' \
---header 'Cookie: _abck=3D9A3157215665FBE57890226BE31027~-1~YAA1'
-
-
-
-Response
-
-{
-    "statusCode": "0000",
-    "statusDescription": "Success",
-    "conversationID": "2f6f4cb969464032a525971dfad534ce",
-    "file": "U2FsdGVkX19Si+5yixbjZh6m"
-}
+Request to GET https://sit1-api.cvshealth.com/file/scan/download/v1/files/1732529951789-screenshot-91e2c38a [DefaultWebClient]
+	*__checkpoint ⇢ Handler com.aetna.asgwy.webmw.api.controller.AVScanFileController#uploadFile(String) [DispatcherHandler]
+	*__checkpoint ⇢ com.aetna.asgwy.webmw.config.CorsFilterConfiguration$$Lambda/0x000002429b70c4b0 [DefaultWebFilterChain]
