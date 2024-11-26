8e87b8cec93c4458a33c8a93b04e2f214367b86f
nsgram <68147300+nsgram@users.noreply.github.com>
2024-11-26 15:16:03 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,8 @@
-Request to GET https://sit1-api.cvshealth.com/file/scan/download/v1/files/1732529951789-screenshot-91e2c38a [DefaultWebClient]
-	*__checkpoint ⇢ Handler com.aetna.asgwy.webmw.api.controller.AVScanFileController#uploadFile(String) [DispatcherHandler]
-	*__checkpoint ⇢ com.aetna.asgwy.webmw.config.CorsFilterConfiguration$$Lambda/0x000002429b70c4b0 [DefaultWebFilterChain]
+curl --location --request POST 'https://sit1-api.cvshealth.com/file/scan/v1/upload' \
+--header 'x-api-key: T1gpDfjoNoNPdqqVfGgR1kw3Rnz0oi6w' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "file":"JVBERi0xLjMKJbrfrOAKMyAwIG9iago8PC9",
+    "state":"teststate",
+    "fileName":"screenshot"
+}'
