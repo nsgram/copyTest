f4be791a0e6b281c0453b34959e5d46cebb2a021
nsgram <68147300+nsgram@users.noreply.github.com>
2024-11-26 15:52:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,29 +1,14 @@
-curl -X 'POST' \
-  'https://devquote-svc.aetna.com/asgwy-api/v1/quote/file' \
-  -H 'accept: */*' \
-  -H 'Content-Type: multipart/form-data' \
-  -F 'docSubcategory=SBC' \
-  -F 'quoteMissinfoDocInd=N' \
-  -F 'quoteConcessDocInd=N' \
-  -F 'uploadedUsrId=N993527' \
-  -F 'docTyp=SG' \
-  -F 'uploadedUsrNm=FirstName LastName' \
-  -F 'docCategory=RC' \
-  -F 'quoteId=369' \
-  -F 'docSize=291' \
-  -F 'docQuoteStage=INITIAL' \
-  -F 'quoteSubmitDocInd=Y' \
-  -F 'quoteConcessionId=21' \
-  -F 'file=@Steps to install CheckMarx Plugin.pdf;type=application/pdf'
- 
-Write call for above using spring boot3 web client in request use below file reference 
-Convert below file string into  -F 'file write complete java code remove sonar and security issue
-
-{
-  "statusCode": "0000",
-  "statusDescription": "Success",
-  "conversationID": "334020c974274ea384921d7f37767b20",
-  "file": "U2FsdGVkX1+ejNriRQZVMfS7T0VwNMtedvibJU+rwc"
+public class FileUploadDto {
+	private long quoteId;
+	private String docTyp;
+	private String docCategory;
+	private String docSubcategory;
+	private Long docSize;
+	private String docQuoteStage;
+	private Character quoteSubmitDocInd;
+	private Character quoteConcessDocInd;
+	private Long quoteConcessionId;
+	private Character quoteMissinfoDocInd;
+	private String uploadedUsrId;
+	private String uploadedUsrNm;
 }
-
-
