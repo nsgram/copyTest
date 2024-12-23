acbdec0beaf0c81346afc02912c7fddbd792662a
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-23 15:11:13 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,25 +1,5 @@
-private AVScanFileResponse downloadAVScanFile(String fileReference) {
-    try {
-        log.info("downloadAVScanFile() Start...");
-        log.info("downloadAVScanFile() fileReference--->" + fileReference);
-
-        // Perform the WebClient call in a blocking-safe manner
-        return proxyWebClient
-                .get()
-                .uri(uploadUrl, fileReference) // Dynamically replace placeholder
-                .header("x-api-key", apiKey)
-                .header("Authorization", "Bearer " + getJwtToken(fileReference))
-                .retrieve()
-                .onStatus(HttpStatus::isError, clientResponse ->
-                        clientResponse.bodyToMono(String.class).flatMap(errorBody -> {
-                            log.error("Error in AVScan download file API: {}", errorBody);
-                            return Mono.error(new AsgwyGlobalException("Error in AVScan download file API"));
-                        })
-                )
-                .bodyToMono(AVScanFileResponse.class)
-                .block(); // Ensure blocking behavior here
-    } catch (Exception e) {
-        log.error("Error in AV download API: {}", e.getMessage(), e);
-        throw new AsgwyGlobalException("Error in AV download API: " + e.getMessage(), e);
-    }
-}
+curl --location --request GET 'https://sit1-api.cvshealth.com/file/scan/download/v1/files/1734598492275-Screenshot_test-2d57cd4a' \
+--header 'x-api-key: T1gpDfjoNoNPdqqVfGgR1kw3Rnz0oi6w' \
+--header 'Authorization: Bearer xK0g' \
+--header 'Cookie: _abck=3D9A3157215665FBE57890226BE31027~-1~YAAQaDsvFwJr6NeRAQAAftQY2gx5A2B3dJEe0Gi53hGnn7PPhQ2qifrgPWNT+TP3eW+lFCRwZN5l3KPJu2splsykaVztH3wXJeYCAWYeCqPasEo9mtS0f+lMAkJyUPDymm32XbjmaSmnXOItWApWyUm8H/IqPwOiqbhcyvam3FfcohVDifkqiJItLCWHzv92i7JLw6FEx7Qt9otDpNpsqkWoPDlACHp9qB5GGTtZXYkdOGy69OkzYz48qKjC276RTGA5F0tQM8PCstxtHyTus77SvLCKaPm/kWCrrFYTjAiTPIKE2hmKBjrnbJadVSdZdNyzsJCwMVkVJa5Zc3MCv866aY6Gg1O8V/ubuhuOBt0LR8ZYdAu+gIEjtVizNsA6qH5caBm3SHTKyFuvH8gJwrD/wfLVYzMW741Rc2o=~-1~-1~-1' \
+--data-raw ''
