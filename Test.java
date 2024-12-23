88f520b875e27c976187af8cd20dc3ec55c9f119
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-23 17:34:14 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,41 +1,55 @@
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.HttpHost;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.conn.HttpHostConnectException;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import javax.net.ssl.SSLContext;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.security.KeyStore;
-import org.springframework.util.ResourceUtils;
+import javax.net.ssl.SSLContext;
 
-@Bean
-public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
-    char[] password = "password".toCharArray();
+@Configuration
+public class RestTemplateConfig {
 
-    SSLContext sslContext = SSLContextBuilder.create()
-            .loadKeyMaterial(keyStore("classpath:cert.jks", password), password)
-            .loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+    private static final String JKS_PATH = "classpath:certificate.jks"; // Path to the JKS certificate
+    private static final String JKS_PASSWORD = "changeit";  // The password for the JKS
 
-    // Create the HttpClient using the SSLContext
-    CloseableHttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
 
-    // Use HttpComponentsClientHttpRequestFactory
-    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(client);
+        // Load the JKS file from resources
+        File jksFile = new File(getClass().getClassLoader().getResource("certificate.jks").getFile());
 
-    // Return the RestTemplate with the custom request factory
-    return builder.requestFactory(() -> factory).build();
-}
+        // Initialize the KeyStore
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(jksFile.toURI().toURL().openStream(), JKS_PASSWORD.toCharArray());
 
-private KeyStore keyStore(String file, char[] password) throws Exception {
-    KeyStore keyStore = KeyStore.getInstance("PKCS12");
-    File key = ResourceUtils.getFile(file);
-    try (InputStream in = new FileInputStream(key)) {
-        keyStore.load(in, password);
+        // Initialize the SSLContext using the KeyStore
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadKeyMaterial(keyStore, JKS_PASSWORD.toCharArray()) // Optionally load the private key
+                .loadTrustMaterial(keyStore, new TrustSelfSignedStrategy()) // Trust all certificates in the JKS
+                .build();
+
+        // Create a custom HttpClient with SSLContext
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(sslSocketFactory)
+                .build();
+
+        // Create HttpComponentsClientHttpRequestFactory with custom HttpClient
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        
+        // Return the RestTemplate
+        return new RestTemplate(factory);
     }
-    return keyStore;
 }
