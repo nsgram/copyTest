3321576bfaaff305f99ee8a033ce41e44dfc91ae
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-23 16:50:22 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,22 +1,16 @@
-@Bean
-public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
-    char[] password = "password".toCharArray();
-
-    SSLContext sslContext = SSLContextBuilder.create()
-            .loadKeyMaterial(keyStore("classpath:cert.jks", password), password)
-            .loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
-
-    HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
-    return builder
-            .requestFactory(new HttpComponentsClientHttpRequestFactory(client))
-            .build();
-}
-
- private KeyStore keyStore(String file, char[] password) throws Exception {
-    KeyStore keyStore = KeyStore.getInstance("PKCS12");
-    File key = ResourceUtils.getFile(file);
-    try (InputStream in = new FileInputStream(key)) {
-        keyStore.load(in, password);
-    }
-    return keyStore;
-}
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.impl.client.HttpClient;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.HttpRequestFactory;
+import org.apache.http.impl.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import javax.net.ssl.SSLContext;
+import org.springframework.util.ResourceUtils;
