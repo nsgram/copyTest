dca99db9d136505442822ebfe0e57a4df42931a5
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-23 16:47:33 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,41 +1,22 @@
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.ssl.SSLContexts;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
+@Bean
+public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
+    char[] password = "password".toCharArray();
 
-import javax.net.ssl.SSLContext;
-import java.io.File;
-import java.io.FileInputStream;
-import java.security.KeyStore;
-import java.util.function.Consumer;
+    SSLContext sslContext = SSLContextBuilder.create()
+            .loadKeyMaterial(keyStore("classpath:cert.jks", password), password)
+            .loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
 
-public class WebClientConfig {
+    HttpClient client = HttpClients.custom().setSSLContext(sslContext).build();
+    return builder
+            .requestFactory(new HttpComponentsClientHttpRequestFactory(client))
+            .build();
+}
 
-    public WebClient webClientWithJks() throws Exception {
-        // Load the keystore
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        try (FileInputStream fis = new FileInputStream(new File("src/main/resources/your-keystore.jks"))) {
-            keyStore.load(fis, "your-keystore-password".toCharArray());
-        }
-
-        // Set up the SSLContext
-        SSLContext sslContext = SSLContexts.custom()
-                .loadKeyMaterial(keyStore, "your-key-password".toCharArray()) // Password for private key
-                .build();
-
-        // Set up the HTTP client with SSL support and default SSL hostname verifier (i.e., skip hostname verification)
-        HttpClient httpClient = HttpClient.create()
-                .secure(ssl -> ssl.sslContext(sslContext).hostnameVerifier(NoopHostnameVerifier.INSTANCE));
-
-        // Create WebClient with reactive HTTP connector
-        WebClient.Builder webClientBuilder = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient));
-
-        return webClientBuilder.build();
+ private KeyStore keyStore(String file, char[] password) throws Exception {
+    KeyStore keyStore = KeyStore.getInstance("PKCS12");
+    File key = ResourceUtils.getFile(file);
+    try (InputStream in = new FileInputStream(key)) {
+        keyStore.load(in, password);
     }
+    return keyStore;
 }
