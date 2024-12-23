fdfb5b9fb7ff8007e8dd06e1b2b765aead202863
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-23 18:29:00 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,55 +1,26 @@
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.HttpHost;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.conn.HttpHostConnectException;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.security.KeyStore;
-import javax.net.ssl.SSLContext;
+// Load the keystore
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			keyStore.load(new FileInputStream(new File("src/main/resources/malware.qa.jks")), "malware".toCharArray());
 
-@Configuration
-public class RestTemplateConfig {
+			// Create SSL context
+			SSLContext sslContext = SSLContextBuilder.create().loadKeyMaterial(keyStore, "malware".toCharArray())
+					.build();
 
-    private static final String JKS_PATH = "classpath:certificate.jks"; // Path to the JKS certificate
-    private static final String JKS_PASSWORD = "changeit";  // The password for the JKS
+			// Create HTTP client with SSL context
+			HttpClient httpClient = HttpClientBuilder.create().setSSLContext(sslContext).build();
 
-    @Bean
-    public RestTemplate restTemplate() throws Exception {
+			// Create request factory
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+					(org.apache.hc.client5.http.classic.HttpClient) httpClient);
 
-        // Load the JKS file from resources
-        File jksFile = new File(getClass().getClassLoader().getResource("certificate.jks").getFile());
+			// Create RestTemplate
+			RestTemplate restTemplate = new RestTemplate(requestFactory);
+			
+			getting below error
 
-        // Initialize the KeyStore
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(jksFile.toURI().toURL().openStream(), JKS_PASSWORD.toCharArray());
 
-        // Initialize the SSLContext using the KeyStore
-        SSLContext sslContext = SSLContextBuilder.create()
-                .loadKeyMaterial(keyStore, JKS_PASSWORD.toCharArray()) // Optionally load the private key
-                .loadTrustMaterial(keyStore, new TrustSelfSignedStrategy()) // Trust all certificates in the JKS
-                .build();
 
-        // Create a custom HttpClient with SSLContext
-        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(sslSocketFactory)
-                .build();
-
-        // Create HttpComponentsClientHttpRequestFactory with custom HttpClient
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        
-        // Return the RestTemplate
-        return new RestTemplate(factory);
-    }
-}
+java.lang.ClassCastException: class org.apache.http.impl.client.InternalHttpClient cannot
+ be cast to class org.apache.hc.client5.http.classic.HttpClient (org.apache.http.impl.client.InternalHttpClient 
+ and org.apache.hc.client5.http.classic.HttpClient are in unnamed module of loader 'app')
