839f16c1fc4f19ef2dd73648cf95a579cc897e6e
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-24 23:09:26 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,48 +1,7 @@
-public static void getProxyResponse(String fileReference, String token) {
-    try {
-        // Proxy setup
-        InetSocketAddress proxyAddress = new InetSocketAddress("proxy.aetna.com", 9119);
-        Proxy proxyDetails = new Proxy(Proxy.Type.HTTP, proxyAddress);
-
-        Authenticator.setDefault(new Authenticator() {
-            @Override
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("N993527", "Java#Java@09".toCharArray());
-            }
-        });
-
-        // URL Connection
-        URL url = new URL("https://sit1-api.cvshealth.com/file/scan/download/v1/files/" + fileReference);
-        HttpsURLConnection httpsURLConn = (HttpsURLConnection) url.openConnection(proxyDetails);
-
-        // Request settings
-        httpsURLConn.setRequestMethod("GET");
-        httpsURLConn.setDoInput(true);
-        httpsURLConn.setUseCaches(true);
-        httpsURLConn.setRequestProperty("x-api-key", "T1gpDfjoNoNPdqqVfGgR1kw3Rnz0oi6w");
-        httpsURLConn.setRequestProperty("Authorization", token);
-
-        // Execute request
-        int responseCode = httpsURLConn.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
-        System.out.println("Response Message: " + httpsURLConn.getResponseMessage());
-
-        // Check if the response is successful
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            try (BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(httpsURLConn.getInputStream()))) {
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    response.append(line).append("\n");
-                }
-                System.out.println("Response Body:\n" + response.toString());
-            }
-        } else {
-            System.err.println("Error: Unable to fetch the file. Response Code: " + responseCode);
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+{"statusCode":"0000",
+"statusDescription":"Success",
+"conversationID":"7e63aaa5edbc4868b",
+"file":"U2F"
 }
+
+above is response String can you create pojo for it set value
