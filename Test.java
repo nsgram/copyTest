9eeffc304e82b0f27c09a165fbcbd3895bc0a120
nsgram <68147300+nsgram@users.noreply.github.com>
2024-12-27 18:27:43 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
-com.aetna.asgwy.webmw.exception.AsgwyGlobalException: Error in AV download api ::org.springframework.web.reactive.function.client.WebClientRequestException: Connection reset
-	at com.aetna.asgwy.webmw.service.AVScanFileServiceV3.downloadAVScanFile(AVScanFileServiceV3.java:177) ~[classes/:na]
-	Suppressed: reactor.core.publisher.FluxOnAssembly$OnAssemblyException: 
-Error has been observed at the following site(s):
-	*__checkpoint ⇢ com.aetna.asgwy.webmw.config.CorsFilterConfiguration$$Lambda/0x000002885a77b000 [DefaultWebFilterChain]
+WebClient.builder()
+    .baseUrl("https://example.com")
+    .clientConnector(new ReactorClientHttpConnector(
+        HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofSeconds(30))
+    ))
+    .build();
