4920fbb94c29e37f38223267111618b2f3e84f60
nsgram <68147300+nsgram@users.noreply.github.com>
2025-01-03 14:19:05 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,18 @@
-WebClient.builder()
-    .baseUrl("https://example.com")
-    .clientConnector(new ReactorClientHttpConnector(
-        HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-            .responseTimeout(Duration.ofSeconds(30))
-    ))
-    .build();
+repo method like below
+
+@Transactional
+	@Modifying
+	@Query(value = "update {h-schema}quote_document qd set doc_status =:newStatus, update_usr_id =:updatedUsrId, update_dts =:updateDts where qd.quote_id =:quoteId and qd.doc_status =:currentStatus ", nativeQuery = true)
+	int updateDocStatus(@Param("quoteId") Long quoteId, @Param("newStatus") String newStatus,
+			@Param("updatedUsrId") String updatedUsrId, @Param("updateDts") Timestamp updateDts,
+			@Param("currentStatus") String currentStatus);
+
+
+Calling test method like below
+Timestamp currentTime = Timestamp.valueOf(LocalDateTime.now(ZoneId.of("America/New_York")));
+verify(quoteDocumentRepository, times(1)).updateDocStatus(anyLong(),anyString(), anyString(), currentTime, anyString());
+
+
+
+getting below error
+
