a6a702f5629d27c53bbf007db3ab6fabd1b72448
nsgram <68147300+nsgram@users.noreply.github.com>
2025-01-03 14:25:45 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,35 +1,18 @@
-repo method like below
-
-@Transactional
-	@Modifying
-	@Query(value = "update {h-schema}quote_document qd set doc_status =:newStatus, update_usr_id =:updatedUsrId, update_dts =:updateDts where qd.quote_id =:quoteId and qd.doc_status =:currentStatus ", nativeQuery = true)
-	int updateDocStatus(@Param("quoteId") Long quoteId, @Param("newStatus") String newStatus,
-			@Param("updatedUsrId") String updatedUsrId, @Param("updateDts") Timestamp updateDts,
-			@Param("currentStatus") String currentStatus);
-
-
-Calling test method like below
-Timestamp currentTime = Timestamp.valueOf(LocalDateTime.now(ZoneId.of("America/New_York")));
-verify(quoteDocumentRepository, times(1)).updateDocStatus(anyLong(),anyString(), anyString(), currentTime, anyString());
-
-
-
-getting below error
-Invalid use of argument matchers!
-5 matchers expected, 4 recorded:
--> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:224)
--> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:224)
--> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:224)
--> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:224)
-
-This exception may occur if matchers are combined with raw values:
-    //incorrect:
-    someMethod(any(), "raw String");
-When using matchers, all arguments have to be provided by matchers.
-For example:
-    //correct:
-    someMethod(any(), eq("String by matcher"));
-
-For more info see javadoc for Matchers class.
-
-	at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:224)
+Argument(s) are different! Wanted:
+quoteDocumentRepository.updateDocStatus(
+    <any long>,
+    <any string>,
+    <any string>,
+    2025-01-03 03:54:08.3921773,
+    <any string>
+);
+-> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:225)
+Actual invocations have different arguments at position [3]:
+quoteDocumentRepository.updateDocStatus(
+    1L,
+    "STAGE_AFS",
+    "Junit",
+    2025-01-03 03:54:08.4491766,
+    "STAGE_TEMP"
+);
+-> at com.aetna.asgwy.service.impl.MissingInfoServiceImpl.updateMissingInfoStatus(MissingInfoServiceImpl.java:106
