9043765bda8ce66076098f102ed226e17a8a7f39
nsgram <68147300+nsgram@users.noreply.github.com>
2025-01-03 14:36:37 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,18 +1,20 @@
+verify(quoteDocumentRepository, times(1)).updateDocStatus(eq(1l),eq("STAGE_AFS"),eq("JUNIT"),eq(currentTime), eq("STAGE_TEMP"));
+
+
 Argument(s) are different! Wanted:
 quoteDocumentRepository.updateDocStatus(
-    <any long>,
-    <any string>,
-    <any string>,
-    2025-01-03 03:54:08.3921773,
-    <any string>
+    1L,
+    "STAGE_AFS",
+    "JUNIT",
+    2025-01-03 04:04:57.3032187,
+    "STAGE_TEMP"
 );
 -> at com.aetna.asgwy.service.MissingInfoServiceImplTest.testUpdateQuoteAndDocStatus_Success(MissingInfoServiceImplTest.java:225)
-Actual invocations have different arguments at position [3]:
+Actual invocations have different arguments at positions [2, 3]:
 quoteDocumentRepository.updateDocStatus(
     1L,
     "STAGE_AFS",
     "Junit",
-    2025-01-03 03:54:08.4491766,
+    2025-01-03 04:04:57.3662193,
     "STAGE_TEMP"
 );
--> at com.aetna.asgwy.service.impl.MissingInfoServiceImpl.updateMissingInfoStatus(MissingInfoServiceImpl.java:106
