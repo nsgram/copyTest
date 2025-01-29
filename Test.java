bcbc784b2c1da28c3016b31ddc3e69fa515114c4
nsgram <68147300+nsgram@users.noreply.github.com>
2025-01-29 21:54:18 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -36,3 +36,13 @@ return chain.filter(exchange.mutate().build())
 
 
 
+
+
+
+
+
+return chain.filter(exchange.mutate().build())
+        .then(Mono.defer(() -> exchange.getResponse().writeWith(
+                Mono.just(exchange.getResponse().bufferFactory()
+                        .wrap(new ObjectMapper().writeValueAsBytes("The uploaded file appears to be unsafe.")))
+        )));
