505f902c3eed62a24e6e5652418cec0b5b89f20d
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-13 23:18:02 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,42 +1,34 @@
-private Mono<FileUploadResponse> handleFileProcessing(String avFileRef, Map<String, Object> decodeJson,
-        AVScanFileRequest avScanFileRequest, ServerWebExchange exchange) {
-    return Mono.using(
-        // Resource Supplier
-        () -> {
-            String avOriginalFileName = (String) decodeJson.get("cvs_av_original_file_name");
-            String avOriginalFileType = (String) decodeJson.get("cvs_av_original_file_type");
-            String tempDir = System.getProperty("java.io.tmpdir");
-            // Write to a temporary file
-            File tempFile = new File(tempDir, avOriginalFileName + "." + avOriginalFileType);
-            AVScanFileResponse avDownloadResponse = downloadAVScanFile(avFileRef);
-            
-            log.info("File Downloaded from AV malware statusDescription::{} ",
-                    avDownloadResponse.getStatusDescription());
-            String avFileDownloadKey = (String) decodeJson.get("cvs_av_file_download_key");
-            
-            String decodedContent = AVScanFileUtil.decryptValue(avDownloadResponse.getFile(),
-                    avFileDownloadKey);
-            byte[] decodedBytes = Base64.getDecoder().decode(decodedContent);
-            String docTitle = docTitle(avScanFileRequest.getUploadData().getQuoteId(),
-                    avScanFileRequest.getUploadData().getDocTyp(),
-                    avScanFileRequest.getUploadData().getDocSubcategory(),
-                    avOriginalFileType);
-            docTitle = uploadFile(decodedBytes, docTitle);
-            
-            try (FileOutputStream fos = new FileOutputStream(tempFile)) {
-                String decodedContent1 = AVScanFileUtil.decryptValue(avDownloadResponse.getFile(),
-                        avFileDownloadKey);
-                byte[] decodedBytes2 = Base64.getDecoder().decode(decodedContent1);
-                fos.write(decodedBytes2);
-            }
-            return docTitle;
-        },
-        // Mono using Resource
-        docTitle -> {
-            log.info("Uploading file to gateway...");
-            FileUploadResponse response = uploadFileOnGateway(null, avScanFileRequest, exchange);
-            log.info("Uploading file to gateway document id..." + response.getQuoteDocumentId());
-            return Mono.just(response);
-        }
-    );
-}
+	private Mono<FileUploadResponse> handleFileProcessing2(String avFileRef, Map<String, Object> decodeJson,
+			AVScanFileRequest avScanFileRequest, ServerWebExchange exchange) {
+		return Mono.using(
+				// Resource Supplier
+				() -> {
+					String avOriginalFileName = (String) decodeJson.get("cvs_av_original_file_name");
+					String avOriginalFileType = (String) decodeJson.get("cvs_av_original_file_type");
+					String tempDir = System.getProperty("java.io.tmpdir");
+					// Write to a temporary file
+					File tempFile = new File(tempDir, avOriginalFileName + "." + avOriginalFileType);
+					AVScanFileResponse avDownloadResponse = downloadAVScanFile(avFileRef);
+
+					log.info("File Downloaded from AV malware statusDescription::{} ",
+							avDownloadResponse.getStatusDescription());
+					String avFileDownloadKey = (String) decodeJson.get("cvs_av_file_download_key");
+
+					String decodedContent = AVScanFileUtil.decryptValue(avDownloadResponse.getFile(),
+							avFileDownloadKey);
+					byte[] decodedBytes = Base64.getDecoder().decode(decodedContent);
+					String docTitle = docTitle(avScanFileRequest.getUploadData().getQuoteId(),
+							avScanFileRequest.getUploadData().getDocTyp(),
+							avScanFileRequest.getUploadData().getDocSubcategory(), avOriginalFileType);
+					docTitle = uploadFile(decodedBytes, docTitle);
+
+					return docTitle;
+				},
+				// Mono using Resource
+				docTitle -> {
+					log.info("Uploading file to gateway...");
+					FileUploadResponse response = uploadFileOnGateway(docTitle, avScanFileRequest, exchange);
+					log.info("Uploading file to gateway document id..." + response.getQuoteDocumentId());
+					return Mono.just(response);
+				});
+	}
