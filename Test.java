d239dea49e42d95d6eca616d024de2a272ce72b7
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-14 10:30:22 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,34 +1,24 @@
-	private Mono<FileUploadResponse> handleFileProcessing2(String avFileRef, Map<String, Object> decodeJson,
-			AVScanFileRequest avScanFileRequest, ServerWebExchange exchange) {
-		return Mono.using(
-				// Resource Supplier
-				() -> {
-					String avOriginalFileName = (String) decodeJson.get("cvs_av_original_file_name");
-					String avOriginalFileType = (String) decodeJson.get("cvs_av_original_file_type");
-					String tempDir = System.getProperty("java.io.tmpdir");
-					// Write to a temporary file
-					File tempFile = new File(tempDir, avOriginalFileName + "." + avOriginalFileType);
-					AVScanFileResponse avDownloadResponse = downloadAVScanFile(avFileRef);
 
-					log.info("File Downloaded from AV malware statusDescription::{} ",
-							avDownloadResponse.getStatusDescription());
-					String avFileDownloadKey = (String) decodeJson.get("cvs_av_file_download_key");
+	private String uploadOnAFSFile(InputStream contentInputStream, String docTitle) throws IOException {
+		//log.info("AzureStorageService.uploadFile :: File upload started on AFS " + file.getOriginalFilename());
+		BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+		BlobClient blobClient = blobContainerClient.getBlobClient(docTitle);
+		try {
+			blobClient.upload(contentInputStream, contentInputStream.available(), true);
+			BlobHttpHeaders headers = new BlobHttpHeaders().setContentType("application/octet-stream");
+			blobClient.setHttpHeaders(headers);
+			log.info("CSV getBlobUrl:{}",blobClient.getBlobUrl());
+		} catch (BlobStorageException e) {
+			log.error("Error uploading file e :: {}", e.getMessage());
+		}
+		log.info("AzureStorageService.uploadFile ::File uploaded AFS:: " + docTitle);
+		return blobClient.getBlobName();
 
-					String decodedContent = AVScanFileUtil.decryptValue(avDownloadResponse.getFile(),
-							avFileDownloadKey);
-					byte[] decodedBytes = Base64.getDecoder().decode(decodedContent);
-					String docTitle = docTitle(avScanFileRequest.getUploadData().getQuoteId(),
-							avScanFileRequest.getUploadData().getDocTyp(),
-							avScanFileRequest.getUploadData().getDocSubcategory(), avOriginalFileType);
-					docTitle = uploadFile(decodedBytes, docTitle);
-
-					return docTitle;
-				},
-				// Mono using Resource
-				docTitle -> {
-					log.info("Uploading file to gateway...");
-					FileUploadResponse response = uploadFileOnGateway(docTitle, avScanFileRequest, exchange);
-					log.info("Uploading file to gateway document id..." + response.getQuoteDocumentId());
-					return Mono.just(response);
-				});
 	}
+
+
+Getting below exception in above method
+
+[2025-02-14 10:24:53.632] asgwy-webmw-oci-dev [WARN ] parallel-1 [N892902]-[]-[1e8391d1]  reactor.core.Exceptions - throwIfFatal detected a jvm fatal exception, which is thrown and logged below:
+java.lang.NoSuchMethodError: 'reactor.core.publisher.Mono reactor.core.publisher.Mono.subscriberContext(reactor.util.context.Context)'
+	at com.azure.storage.blob.BlobClient.uploadWithResponse(BlobClient.java:303)
