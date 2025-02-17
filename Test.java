ff3ca2667be84b16b00ac4b9ecef2ef8e63d904e
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-17 20:03:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,24 +1,19 @@
+8.	Please provide nominated associate’s primary roles and responsibility on this U.S. project
+I have the below sample
+•	Meet with the business team to identify software automation opportunities and gather requirements. Evaluate the business requirements and analyze them to recommend software automation solutions using Robotic Process Automation (RPA) and similar innovative technologies.
+•	Prepare functional and technical documentation that includes complex business requirements, system design specifications, required automation tool details, technical requirements, security systems requirements, process architecture, and testing methodologies.
+•	Analyze, design, architect, and develop software automation solutions using the RPA UiPath tool. Customize the automation solutions using high-level programming languages coding. Test the automation processes and jobs. Use the UiPath Orchestrator tool to run the automation processes, maintain secured passwords and assets, add and delete queue items, and schedule the automation jobs.
+•	Prepare the documents, software processes, and jobs for the final production deployment. Provide hyper-care support and after-deployment production support. Attend project team meetings to understand process change requests from business owners and provide software automation solutions.
+•	Utilize the application knowledge and the RPA technology knowledge to identify the process modifications and provide solutions for the process optimizations to improve productivity.
+below are My roles and responsibility
 
-	private String uploadOnAFSFile(InputStream contentInputStream, String docTitle) throws IOException {
-		//log.info("AzureStorageService.uploadFile :: File upload started on AFS " + file.getOriginalFilename());
-		BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
-		BlobClient blobClient = blobContainerClient.getBlobClient(docTitle);
-		try {
-			blobClient.upload(contentInputStream, contentInputStream.available(), true);
-			BlobHttpHeaders headers = new BlobHttpHeaders().setContentType("application/octet-stream");
-			blobClient.setHttpHeaders(headers);
-			log.info("CSV getBlobUrl:{}",blobClient.getBlobUrl());
-		} catch (BlobStorageException e) {
-			log.error("Error uploading file e :: {}", e.getMessage());
-		}
-		log.info("AzureStorageService.uploadFile ::File uploaded AFS:: " + docTitle);
-		return blobClient.getBlobName();
-
-	}
+Involved in to understanding project requirements and implementing effective technical solutions while continuously enhancing skills.
+It includes developing efficient code in Angular and Java, designing database schemas, and building front-end components.
+Responsibilities also include collaborating with teams for seamless system integration, troubleshooting issues, and ensuring performance optimization.
+Post-production maintenance is handled with a focus on reliability and scalability.
+Additionally, involved in to mentoring team members, fostering collaboration, and leading technical aspects with accountability.
+Software specification documents are prepared for each API to ensure clarity, maintainability, and alignment with industry standards, contributing to overall project success.
 
 
-Getting below exception in above method
-
-[2025-02-14 10:24:53.632] asgwy-webmw-oci-dev [WARN ] parallel-1 [N892902]-[]-[1e8391d1]  reactor.core.Exceptions - throwIfFatal detected a jvm fatal exception, which is thrown and logged below:
-java.lang.NoSuchMethodError: 'reactor.core.publisher.Mono reactor.core.publisher.Mono.subscriberContext(reactor.util.context.Context)'
-	at com.azure.storage.blob.BlobClient.uploadWithResponse(BlobClient.java:303)
+it should be in below criteria with point wise
+Qualified level position: requires a good understanding of the job duties/ job requirement. Senior Associates perform moderately complex tasks that require limited judgment. Managerial responsibilities/ tasks, team lead responsibilities are NOT permitted.
