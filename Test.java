34e91129ce1dffbcfb98d9a81892702aa4383b7e
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-20 18:44:02 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,168 @@
-Job Duties
-1	Participating in the requirement gathering sessions, provide analysis support and track them using IBM Rational Jazz Team Server. Review of existing systems configurations and operating methods as well as understanding evolving business needs. 
-2	Attending high-level design solution sessions with Enterprise solution Architects and creating Application design specification documents, storing them in SharePoint. This phase establishes the framework for each phase of the software development lifecycle and a miss could impact the entire software solution.
-3	Solutioning, designing, programming and unit testing various applications/services using Cognizant tools and best practices/methods which would also include analyzing and achieving quality engineering and assurance of Quoting Center Core Framework Applications using including Java/J2ee, Spring Boot, Microservices, SQL Programming, SOAP, REST API, JDBC.
-4	Collaborating with Product Owners and Application Architects to understand business requirements for existing/ new software applications/ tools. Understanding the features (business requirements) by participating in the PI (Program Increment) overview meetings and breaking them into user stories with granular tasks and estimates. Attending PI Planning and contributing to the distribution of user stories across the sprints, Review Epics/User Stories with Product Owners and Quoting Business team and identifying gaps. Revisiting the Epics/User Stories to fix the gaps.
-5	Procuring appropriate approvals for Epics/User Stories contracts from Application Architecture teams. Exploring multiple design options and choosing optimal and best high-level/ low-level design. Reviewing designs and solutions with the customers/business teams and provide justifications for the best design during the reviews. Also responsible for demonstrations to end users during sprint/program demos.
-6	Participating in technical application support and defect fixing for Quoting Center team. Ensuring that the End Users face minimum disruptions and have continuity of business processes. Solving the critical application issues due to code and functional changes. Performing the technical analysis for the reported defects for the associated applications and interacting with stakeholders for re-design/code refactoring and get their buy-in to address requirement changes and minimize technical debt. Fixing defects raised in staging environments generated due to data inconsistencies. Analyzing and providing Root Cause Analysis (RCA) to management for major incidents.  Checking service health and troubleshooting issues in the cluster environment periodically.
-7	Addressing Code Quality & Deployments which would involve ensuring 90% code coverage using Sonar, running scans on Source code to identify potential issues using Eclipse - PMD (Source Code Analyzer tool), Performing code check-in into GitHub daily, Performing CI/CD deployments to staging environment end of every sprint, Performing CI/CD deployments to higher environments.
-8	Building the Team functionally to code API for enhancing new features of the frameworks, developing state-of-the-art development tools, and deploying projects via DevOps methodology using tools like Apache Maven, Jenkins build tool, Git-Hub source control and IBM Urban Code Deployment.
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class QuotesReportEntity {
+	
+	
+	@Id
+	@Column(name = "dummy_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long dummyId;
+
+	// Groups
+	@Column(name = "group_nm")
+	private String groupNm;
+	@Column(name = "employer_id_nbr")
+	private String employerIdNbr;
+	@Column(name = "test_group_ind")
+	private Boolean testGroupInd;
+
+	// Quotes
+	@Column(name = "quote_id")
+	private Long quoteId;
+	@Column(name = "quote_status_cd")
+	private String quoteStatusCd;
+	@Column(name = "effective_dt")
+	private Date effectiveDt;
+	@Column(name = "group_zip_cd")
+	private String groupZipCd;
+	@Column(name = "group_state_cd")
+	private String groupStateCd;
+	@Column(name = "eligible_entrd_cnt")
+	private Integer eligibleEntrdCnt;
+	@Column(name = "union_emp_ind")
+	private String unionEmpInd;
+	@Column(name = "union_emp_cnt")
+	private Integer unionEmpCnt;
+
+	// quote_product
+	@Column(name = "product_cd")
+	private String productCd;
+
+	// Coverage
+	@Column(name = "group_loc_addr_line1_txt")
+	private String groupLocAddrLine1Txt;
+	@Column(name = "group_loc_addr_line2_txt")
+	private String groupLocAddrLine2Txt;
+	@Column(name = "group_loc_city_nm")
+	private String groupLocCityNm;
+	@Column(name = "curr_carrier_typ_desc")
+	private String currCarrierTypDesc;
+	@Column(name = "curr_med_carrier_nm")
+	private String currMedCarrierNm;
+	@Column(name = "grp_mewa_ind")
+	private String grpMewaAssoc;
+	@Column(name = "aetna_peo_ind")
+	private String aetnaPeoInd;
+	@Column(name = "contract_period_mo_nbr")
+	private Integer contractPeriodMoNbr;
+	@Column(name = "tot_avg_emp_cnt")
+	private Integer totAvgEmpCnt;
+	@Column(name = "sic_cd")
+	private String sicCd;
+	@Column(name = "sic_nm")
+	private String sicNm;
+	@Column(name = "def_broker_fee_amt")
+	private BigDecimal defBrokerFeeAmt;
+	@Column(name = "participation_cnt")
+	private Integer participationCnt;
+	@Column(name = "eligible_dervd_cnt")
+	private Integer eligibleDervdCnt;
+	@Column(name = "waiver_cnt")
+	private Integer waiverCnt;
+	@Column(name = "eligible_ret_cnt")
+	private Integer eligibleRetCnt;
+	@Column(name = "cobra_emp_cnt")
+	private Integer cobraEmpCnt;
+	@Column(name = "participation_pct")
+	private BigDecimal participationPct;
+	@Column(name = "ft_eqvlnt_cnt")
+	private Integer ftEqvlntCnt;
+	@Column(name = "erisa_cd")
+	private String erisaCd;
+	@Column(name = "curr_tpa_nm")
+	private String currTpaNm;
+
+	// quote_concession
+	@Column(name = "concess_req_status_cd")
+	private String concessReqStatusCd;
+	@Column(name = "concess_req_pct")
+	private BigDecimal concessReqPct;
+	@Column(name = "concess_req_reason_txt")
+	private String concessReqReasonTxt;
+	@Column(name = "conn_create_dts")
+	private Timestamp conn_create_dts;
+
+	// QuoteDocument
+	@Column(name = "comment_txt")
+	private String commentTxt;
+
+	// quote_selling_agent
+	@Column(name = "quote_sa_full_nm")
+	private String quotesSAFullNm;
+	@Column(name = "quote_sa_npn")
+	private String quoteSANPN;
+	@Column(name = "quote_sa_agency_nm")
+	private String quoteSAAgencyNm;
+	@Column(name = "quote_sa_cfo_nm")
+	private String quoteSACfoNm;
+	@Column(name = "quote_sa_cfo_loc")
+	private String quoteSACfoLoc;
+
+	// quote_general_agency
+	@Column(name = "quote_ga_agency_nm")
+	private String quoteGAAgencyNm;
+	@Column(name = "quote_ga_sa_cfo_nm")
+	private String quoteGASACfoNm;
+	@Column(name = "quote_ga_sa_cfo_loc")
+	private String quoteGASACfoLoc;
+
+	// quote_sales_exec
+	@Column(name = "quote_sales_exec_first_name")
+	private String quoteSalesExecFirstName;
+	@Column(name = "quote_sales_exec_last_name")
+	private String quoteSalesExecLastName;
+
+	// Below attribute are Not used in report but required for JPA
+	@Column(name = "status_desc")
+	private String statusDesc;
+
+	@Column(name = "create_dts")
+	private Date createDts;
+}
+	I have below rowpappert but it getting null vaules for somet of columns
+	
+	//Quote report
+	public List<QuotesReportEntity> getReportsDataForCSV(RequestDetailsDTO requestDetailsDTO) throws SQLException {
+		StringBuilder sqlQuery = new StringBuilder();
+		List<String> queryArgs = new ArrayList<>();
+		sqlQuery.append("select * from quotes_report_view where 1=1 ");
+		if (requestDetailsDTO.getFromEffectiveDt() != null && !requestDetailsDTO.getFromEffectiveDt().isEmpty()) {
+			sqlQuery.append(" AND effective_dt BETWEEN ?::date and ?::date ");
+			queryArgs.add(requestDetailsDTO.getFromEffectiveDt());
+			queryArgs.add(requestDetailsDTO.getToEffectiveDt());
+		}
+		if (requestDetailsDTO.getFromSubmissionDt() != null && !requestDetailsDTO.getFromSubmissionDt().isEmpty()) {
+			sqlQuery.append(" AND submission_dt BETWEEN ?::date and ?::date ");
+			queryArgs.add(requestDetailsDTO.getFromSubmissionDt());
+			queryArgs.add(requestDetailsDTO.getToSubmissionDt());
+		}
+		if (requestDetailsDTO.getStateCd() != null && !requestDetailsDTO.getStateCd().isEmpty()) {
+			sqlQuery.append(" AND group_state_cd =?");
+			queryArgs.add(requestDetailsDTO.getStateCd());
+		}
+		if (requestDetailsDTO.getStatusDesc() != null && !requestDetailsDTO.getStatusDesc().isEmpty()) {
+			sqlQuery.append(" AND status_desc = ?");
+			queryArgs.add(requestDetailsDTO.getStatusDesc());
+		}
+		Object[] preparedStatementArgs = new Object[queryArgs.size()];
+		for (int i = 0; i < preparedStatementArgs.length; i++) {
+			preparedStatementArgs[i] = queryArgs.get(i);
+		}
+		log.info(" CSV sqlQuery:{}", sqlQuery.toString());
+		return jdbcTemplate.query(sqlQuery.toString(), new BeanPropertyRowMapper<>(QuotesReportEntity.class),
+				preparedStatementArgs);
+	}
