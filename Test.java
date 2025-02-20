a1c5e91f3873cfece87e2d6426c76cd8413eb7c9
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-20 19:03:30 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,168 +1,108 @@
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@ToString
-public class QuotesReportEntity {
-	
-	
-	@Id
-	@Column(name = "dummy_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long dummyId;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-	// Groups
-	@Column(name = "group_nm")
-	private String groupNm;
-	@Column(name = "employer_id_nbr")
-	private String employerIdNbr;
-	@Column(name = "test_group_ind")
-	private Boolean testGroupInd;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	// Quotes
-	@Column(name = "quote_id")
-	private Long quoteId;
-	@Column(name = "quote_status_cd")
-	private String quoteStatusCd;
-	@Column(name = "effective_dt")
-	private Date effectiveDt;
-	@Column(name = "group_zip_cd")
-	private String groupZipCd;
-	@Column(name = "group_state_cd")
-	private String groupStateCd;
-	@Column(name = "eligible_entrd_cnt")
-	private Integer eligibleEntrdCnt;
-	@Column(name = "union_emp_ind")
-	private String unionEmpInd;
-	@Column(name = "union_emp_cnt")
-	private Integer unionEmpCnt;
+@Repository
+@Slf4j
+public class QuotesReportRepository {
 
-	// quote_product
-	@Column(name = "product_cd")
-	private String productCd;
+    private final JdbcTemplate jdbcTemplate;
 
-	// Coverage
-	@Column(name = "group_loc_addr_line1_txt")
-	private String groupLocAddrLine1Txt;
-	@Column(name = "group_loc_addr_line2_txt")
-	private String groupLocAddrLine2Txt;
-	@Column(name = "group_loc_city_nm")
-	private String groupLocCityNm;
-	@Column(name = "curr_carrier_typ_desc")
-	private String currCarrierTypDesc;
-	@Column(name = "curr_med_carrier_nm")
-	private String currMedCarrierNm;
-	@Column(name = "grp_mewa_ind")
-	private String grpMewaAssoc;
-	@Column(name = "aetna_peo_ind")
-	private String aetnaPeoInd;
-	@Column(name = "contract_period_mo_nbr")
-	private Integer contractPeriodMoNbr;
-	@Column(name = "tot_avg_emp_cnt")
-	private Integer totAvgEmpCnt;
-	@Column(name = "sic_cd")
-	private String sicCd;
-	@Column(name = "sic_nm")
-	private String sicNm;
-	@Column(name = "def_broker_fee_amt")
-	private BigDecimal defBrokerFeeAmt;
-	@Column(name = "participation_cnt")
-	private Integer participationCnt;
-	@Column(name = "eligible_dervd_cnt")
-	private Integer eligibleDervdCnt;
-	@Column(name = "waiver_cnt")
-	private Integer waiverCnt;
-	@Column(name = "eligible_ret_cnt")
-	private Integer eligibleRetCnt;
-	@Column(name = "cobra_emp_cnt")
-	private Integer cobraEmpCnt;
-	@Column(name = "participation_pct")
-	private BigDecimal participationPct;
-	@Column(name = "ft_eqvlnt_cnt")
-	private Integer ftEqvlntCnt;
-	@Column(name = "erisa_cd")
-	private String erisaCd;
-	@Column(name = "curr_tpa_nm")
-	private String currTpaNm;
+    public QuotesReportRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	// quote_concession
-	@Column(name = "concess_req_status_cd")
-	private String concessReqStatusCd;
-	@Column(name = "concess_req_pct")
-	private BigDecimal concessReqPct;
-	@Column(name = "concess_req_reason_txt")
-	private String concessReqReasonTxt;
-	@Column(name = "conn_create_dts")
-	private Timestamp conn_create_dts;
+    public List<QuotesReportEntity> getReportsDataForCSV(RequestDetailsDTO requestDetailsDTO) throws SQLException {
+        StringBuilder sqlQuery = new StringBuilder();
+        List<Object> queryArgs = new ArrayList<>();
 
-	// QuoteDocument
-	@Column(name = "comment_txt")
-	private String commentTxt;
+        sqlQuery.append("SELECT * FROM quotes_report_view WHERE 1=1");
 
-	// quote_selling_agent
-	@Column(name = "quote_sa_full_nm")
-	private String quotesSAFullNm;
-	@Column(name = "quote_sa_npn")
-	private String quoteSANPN;
-	@Column(name = "quote_sa_agency_nm")
-	private String quoteSAAgencyNm;
-	@Column(name = "quote_sa_cfo_nm")
-	private String quoteSACfoNm;
-	@Column(name = "quote_sa_cfo_loc")
-	private String quoteSACfoLoc;
+        if (requestDetailsDTO.getFromEffectiveDt() != null && !requestDetailsDTO.getFromEffectiveDt().isEmpty()) {
+            sqlQuery.append(" AND effective_dt BETWEEN CAST(? AS DATE) AND CAST(? AS DATE)");
+            queryArgs.add(requestDetailsDTO.getFromEffectiveDt());
+            queryArgs.add(requestDetailsDTO.getToEffectiveDt());
+        }
+        if (requestDetailsDTO.getFromSubmissionDt() != null && !requestDetailsDTO.getFromSubmissionDt().isEmpty()) {
+            sqlQuery.append(" AND submission_dt BETWEEN CAST(? AS DATE) AND CAST(? AS DATE)");
+            queryArgs.add(requestDetailsDTO.getFromSubmissionDt());
+            queryArgs.add(requestDetailsDTO.getToSubmissionDt());
+        }
+        if (requestDetailsDTO.getStateCd() != null && !requestDetailsDTO.getStateCd().isEmpty()) {
+            sqlQuery.append(" AND group_state_cd = ?");
+            queryArgs.add(requestDetailsDTO.getStateCd());
+        }
+        if (requestDetailsDTO.getStatusDesc() != null && !requestDetailsDTO.getStatusDesc().isEmpty()) {
+            sqlQuery.append(" AND status_desc = ?");
+            queryArgs.add(requestDetailsDTO.getStatusDesc());
+        }
 
-	// quote_general_agency
-	@Column(name = "quote_ga_agency_nm")
-	private String quoteGAAgencyNm;
-	@Column(name = "quote_ga_sa_cfo_nm")
-	private String quoteGASACfoNm;
-	@Column(name = "quote_ga_sa_cfo_loc")
-	private String quoteGASACfoLoc;
+        log.info("Generated SQL Query: {}", sqlQuery);
 
-	// quote_sales_exec
-	@Column(name = "quote_sales_exec_first_name")
-	private String quoteSalesExecFirstName;
-	@Column(name = "quote_sales_exec_last_name")
-	private String quoteSalesExecLastName;
+        return jdbcTemplate.query(sqlQuery.toString(), (rs, rowNum) -> mapResultSetToEntity(rs), queryArgs.toArray());
+    }
 
-	// Below attribute are Not used in report but required for JPA
-	@Column(name = "status_desc")
-	private String statusDesc;
+    private QuotesReportEntity mapResultSetToEntity(ResultSet rs) throws SQLException {
+        QuotesReportEntity entity = new QuotesReportEntity();
+        
+        // Mapping fields from ResultSet to entity
+        entity.setDummyId(rs.getLong("dummy_id"));
+        entity.setGroupNm(rs.getString("group_nm"));
+        entity.setEmployerIdNbr(rs.getString("employer_id_nbr"));
+        entity.setTestGroupInd(rs.getBoolean("test_group_ind"));
+        entity.setQuoteId(rs.getLong("quote_id"));
+        entity.setQuoteStatusCd(rs.getString("quote_status_cd"));
+        entity.setEffectiveDt(rs.getDate("effective_dt"));
+        entity.setGroupZipCd(rs.getString("group_zip_cd"));
+        entity.setGroupStateCd(rs.getString("group_state_cd"));
+        entity.setEligibleEntrdCnt(rs.getInt("eligible_entrd_cnt"));
+        entity.setUnionEmpInd(rs.getString("union_emp_ind"));
+        entity.setUnionEmpCnt(rs.getInt("union_emp_cnt"));
+        entity.setProductCd(rs.getString("product_cd"));
+        entity.setGroupLocAddrLine1Txt(rs.getString("group_loc_addr_line1_txt"));
+        entity.setGroupLocAddrLine2Txt(rs.getString("group_loc_addr_line2_txt"));
+        entity.setGroupLocCityNm(rs.getString("group_loc_city_nm"));
+        entity.setCurrCarrierTypDesc(rs.getString("curr_carrier_typ_desc"));
+        entity.setCurrMedCarrierNm(rs.getString("curr_med_carrier_nm"));
+        entity.setGrpMewaAssoc(rs.getString("grp_mewa_ind"));
+        entity.setAetnaPeoInd(rs.getString("aetna_peo_ind"));
+        entity.setContractPeriodMoNbr(rs.getInt("contract_period_mo_nbr"));
+        entity.setTotAvgEmpCnt(rs.getInt("tot_avg_emp_cnt"));
+        entity.setSicCd(rs.getString("sic_cd"));
+        entity.setSicNm(rs.getString("sic_nm"));
+        entity.setDefBrokerFeeAmt(rs.getBigDecimal("def_broker_fee_amt"));
+        entity.setParticipationCnt(rs.getInt("participation_cnt"));
+        entity.setEligibleDervdCnt(rs.getInt("eligible_dervd_cnt"));
+        entity.setWaiverCnt(rs.getInt("waiver_cnt"));
+        entity.setEligibleRetCnt(rs.getInt("eligible_ret_cnt"));
+        entity.setCobraEmpCnt(rs.getInt("cobra_emp_cnt"));
+        entity.setParticipationPct(rs.getBigDecimal("participation_pct"));
+        entity.setFtEqvlntCnt(rs.getInt("ft_eqvlnt_cnt"));
+        entity.setErisaCd(rs.getString("erisa_cd"));
+        entity.setCurrTpaNm(rs.getString("curr_tpa_nm"));
+        entity.setConcessReqStatusCd(rs.getString("concess_req_status_cd"));
+        entity.setConcessReqPct(rs.getBigDecimal("concess_req_pct"));
+        entity.setConcessReqReasonTxt(rs.getString("concess_req_reason_txt"));
+        entity.setConn_create_dts(rs.getTimestamp("conn_create_dts"));
+        entity.setCommentTxt(rs.getString("comment_txt"));
+        entity.setQuotesSAFullNm(rs.getString("quote_sa_full_nm"));
+        entity.setQuoteSANPN(rs.getString("quote_sa_npn"));
+        entity.setQuoteSAAgencyNm(rs.getString("quote_sa_agency_nm"));
+        entity.setQuoteSACfoNm(rs.getString("quote_sa_cfo_nm"));
+        entity.setQuoteSACfoLoc(rs.getString("quote_sa_cfo_loc"));
+        entity.setQuoteGAAgencyNm(rs.getString("quote_ga_agency_nm"));
+        entity.setQuoteGASACfoNm(rs.getString("quote_ga_sa_cfo_nm"));
+        entity.setQuoteGASACfoLoc(rs.getString("quote_ga_sa_cfo_loc"));
+        entity.setQuoteSalesExecFirstName(rs.getString("quote_sales_exec_first_name"));
+        entity.setQuoteSalesExecLastName(rs.getString("quote_sales_exec_last_name"));
+        entity.setStatusDesc(rs.getString("status_desc"));
+        entity.setCreateDts(rs.getDate("create_dts"));
 
-	@Column(name = "create_dts")
-	private Date createDts;
+        return entity;
+    }
 }
-	I have below rowpappert but it getting null vaules for somet of columns
-	
-	//Quote report
-	public List<QuotesReportEntity> getReportsDataForCSV(RequestDetailsDTO requestDetailsDTO) throws SQLException {
-		StringBuilder sqlQuery = new StringBuilder();
-		List<String> queryArgs = new ArrayList<>();
-		sqlQuery.append("select * from quotes_report_view where 1=1 ");
-		if (requestDetailsDTO.getFromEffectiveDt() != null && !requestDetailsDTO.getFromEffectiveDt().isEmpty()) {
-			sqlQuery.append(" AND effective_dt BETWEEN ?::date and ?::date ");
-			queryArgs.add(requestDetailsDTO.getFromEffectiveDt());
-			queryArgs.add(requestDetailsDTO.getToEffectiveDt());
-		}
-		if (requestDetailsDTO.getFromSubmissionDt() != null && !requestDetailsDTO.getFromSubmissionDt().isEmpty()) {
-			sqlQuery.append(" AND submission_dt BETWEEN ?::date and ?::date ");
-			queryArgs.add(requestDetailsDTO.getFromSubmissionDt());
-			queryArgs.add(requestDetailsDTO.getToSubmissionDt());
-		}
-		if (requestDetailsDTO.getStateCd() != null && !requestDetailsDTO.getStateCd().isEmpty()) {
-			sqlQuery.append(" AND group_state_cd =?");
-			queryArgs.add(requestDetailsDTO.getStateCd());
-		}
-		if (requestDetailsDTO.getStatusDesc() != null && !requestDetailsDTO.getStatusDesc().isEmpty()) {
-			sqlQuery.append(" AND status_desc = ?");
-			queryArgs.add(requestDetailsDTO.getStatusDesc());
-		}
-		Object[] preparedStatementArgs = new Object[queryArgs.size()];
-		for (int i = 0; i < preparedStatementArgs.length; i++) {
-			preparedStatementArgs[i] = queryArgs.get(i);
-		}
-		log.info(" CSV sqlQuery:{}", sqlQuery.toString());
-		return jdbcTemplate.query(sqlQuery.toString(), new BeanPropertyRowMapper<>(QuotesReportEntity.class),
-				preparedStatementArgs);
-	}
