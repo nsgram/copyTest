340214ca22d035aef83637a42044fe1f972330e2
nsgram <68147300+nsgram@users.noreply.github.com>
2025-02-25 18:17:00 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -30,118 +30,122 @@ import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
 public class AVScanFileUtil {
-	private static final String pemKey = "-----BEGIN RSA PRIVATE KEY-----\n"
-			+ "jsBQMakCgYEAyOQauRfM2UZAVMb0ohS25Xa6OcSQQfFSiH4rZ1IFklWP7c/Qoc+2\n"
-			+ "Vf4oi/gAzZyDf0/4aX31f6kVoUh74RF4x6nVhKRCQW6lAsx7oJ0hdRSS+FO4blRd\n"
-			+ "/riKn5Rp2RHo+lFl1ujS4abnAkLAhoii4M4Uo1GxYW0S7jj/PAud6RE=\n-----END RSA PRIVATE KEY-----";
+    
+    private static final String PRIVATE_KEY_ENV = "ENCRYPTION_PRIVATE_KEY";
 
-	static {
-		Security.addProvider(new BouncyCastleProvider());
-	}
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
-	public static PrivateKey readPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-		//String filePath = "src/main/resources/encryptionKeys/recipient-np-private-key-asg.pem";
-		//String privateKeyPEM = new String(Files.readAllBytes(Paths.get(filePath)));
-		String privateKeyPEM = new String(pemKey);
+    public static PrivateKey readPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String privateKeyPEM = System.getenv(PRIVATE_KEY_ENV);
+        
+        if (privateKeyPEM == null || privateKeyPEM.isEmpty()) {
+            throw new IllegalStateException("Private key is not set in environment variables.");
+        }
 
-		if (privateKeyPEM.contains("BEGIN RSA PRIVATE KEY")) {
-			return readPKCS1PrivateKey(pemKey);
-		} else {
-			return readPKCS8PrivateKey(privateKeyPEM);
-		}
-	}
+        if (privateKeyPEM.contains("BEGIN RSA PRIVATE KEY")) {
+            return readPKCS1PrivateKey(privateKeyPEM);
+        } else {
+            return readPKCS8PrivateKey(privateKeyPEM);
+        }
+    }
 
-	public static String decryptValue(String encryptedString, String key)
-			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-			InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-		String decryptedText = "";
-		byte[] cipherData = Base64.getDecoder().decode(encryptedString);
-		byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
+    public static String decryptValue(String encryptedString, String key)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        
+        byte[] cipherData = Base64.getDecoder().decode(encryptedString);
+        byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
 
-		MessageDigest md5 = MessageDigest.getInstance("MD5");
-		final byte[][] keyAndIV = GenerateKeyAndIV(32, 16, 1, saltData, key.getBytes(StandardCharsets.UTF_8), md5);
-		SecretKeySpec keySpec = new SecretKeySpec(keyAndIV[0], "AES");
-		IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        final byte[][] keyAndIV = generateKeyAndIV(32, 16, 1, saltData, key.getBytes(StandardCharsets.UTF_8), md5);
+        
+        SecretKeySpec keySpec = new SecretKeySpec(keyAndIV[0], "AES");
+        IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
 
-		byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
-		Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		aesCBC.init(Cipher.DECRYPT_MODE, keySpec, iv);
-		byte[] decryptedData = aesCBC.doFinal(encrypted);
-		decryptedText = new String(decryptedData, StandardCharsets.UTF_8);
+        byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
+        Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        aesCBC.init(Cipher.DECRYPT_MODE, keySpec, iv);
+        
+        byte[] decryptedData = aesCBC.doFinal(encrypted);
+        
+        // Clear sensitive data
+        Arrays.fill(encrypted, (byte) 0);
+        Arrays.fill(saltData, (byte) 0);
 
-		return decryptedText;
-	}
+        return new String(decryptedData, StandardCharsets.UTF_8);
+    }
 
-	private static PrivateKey readPKCS1PrivateKey(String pemKey) throws IOException {
-		try (PEMParser pemParser = new PEMParser(new StringReader(pemKey))) {
-			Object object = pemParser.readObject();
+    private static PrivateKey readPKCS1PrivateKey(String pemKey) throws IOException {
+        try (PEMParser pemParser = new PEMParser(new StringReader(pemKey))) {
+            Object object = pemParser.readObject();
 
-			if (object instanceof PEMKeyPair) {
-				JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-				return converter.getPrivateKey(((PEMKeyPair) object).getPrivateKeyInfo());
-			} else if (object instanceof PrivateKeyInfo) {
-				JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-				return converter.getPrivateKey((PrivateKeyInfo) object);
-			} else {
-				throw new IOException("Unsupported key format.");
-			}
-		}
-	}
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+            if (object instanceof PEMKeyPair) {
+                return converter.getPrivateKey(((PEMKeyPair) object).getPrivateKeyInfo());
+            } else if (object instanceof PrivateKeyInfo) {
+                return converter.getPrivateKey((PrivateKeyInfo) object);
+            } else {
+                throw new IOException("Unsupported key format.");
+            }
+        }
+    }
 
-	private static PrivateKey readPKCS8PrivateKey(String privateKeyPEM)
-			throws NoSuchAlgorithmException, InvalidKeySpecException {
-		privateKeyPEM = privateKeyPEM.replace("-----BEGIN PRIVATE KEY-----", "")
-				.replace("-----END PRIVATE KEY-----", "").replaceAll("\\s+", "");
+    private static PrivateKey readPKCS8PrivateKey(String privateKeyPEM)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        privateKeyPEM = privateKeyPEM.replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "").replaceAll("\\s+", "");
 
-		byte[] keyBytes = Base64.getDecoder().decode(privateKeyPEM);
-		PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-		return keyFactory.generatePrivate(keySpec);
-	}
+        byte[] keyBytes = Base64.getDecoder().decode(privateKeyPEM);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        
+        // Clear sensitive data
+        Arrays.fill(keyBytes, (byte) 0);
 
-	private static byte[][] GenerateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password,
-			MessageDigest md) {
+        return keyFactory.generatePrivate(keySpec);
+    }
 
-		int digestLength = md.getDigestLength();
-		int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
-		byte[] generatedData = new byte[requiredLength];
-		int generatedLength = 0;
+    private static byte[][] generateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password,
+                                             MessageDigest md) {
 
-		try {
-			md.reset();
-			// Repeat process until sufficient data has been generated
-			while (generatedLength < keyLength + ivLength) {
+        int digestLength = md.getDigestLength();
+        int requiredLength = ((keyLength + ivLength + digestLength - 1) / digestLength) * digestLength;
+        byte[] generatedData = new byte[requiredLength];
+        int generatedLength = 0;
 
-				// Digest data (last digest if available, password data, salt if available)
-				if (generatedLength > 0)
-					md.update(generatedData, generatedLength - digestLength, digestLength);
-				md.update(password);
-				if (salt != null)
-					md.update(salt, 0, 8);
-				md.digest(generatedData, generatedLength, digestLength);
+        try {
+            md.reset();
+            while (generatedLength < keyLength + ivLength) {
+                if (generatedLength > 0) {
+                    md.update(generatedData, generatedLength - digestLength, digestLength);
+                }
+                md.update(password);
+                if (salt != null) {
+                    md.update(salt, 0, 8);
+                }
+                md.digest(generatedData, generatedLength, digestLength);
 
-				// additional rounds
-				for (int i = 1; i < iterations; i++) {
-					md.update(generatedData, generatedLength, digestLength);
-					md.digest(generatedData, generatedLength, digestLength);
-				}
+                for (int i = 1; i < iterations; i++) {
+                    md.update(generatedData, generatedLength, digestLength);
+                    md.digest(generatedData, generatedLength, digestLength);
+                }
 
-				generatedLength += digestLength;
-			}
+                generatedLength += digestLength;
+            }
 
-			// Copy key and IV into separate byte arrays
-			byte[][] result = new byte[2][];
-			result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
-			if (ivLength > 0)
-				result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+            byte[][] result = new byte[2][];
+            result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
+            if (ivLength > 0) {
+                result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+            }
 
-			return result;
-
-		} catch (DigestException e) {
-			throw new RuntimeException(e);
-		} finally {
-			// Clean out temporary data
-			Arrays.fill(generatedData, (byte) 0);
-		}
-	}
+            return result;
+        } catch (DigestException e) {
+            throw new RuntimeException("Error generating key and IV", e);
+        } finally {
+            Arrays.fill(generatedData, (byte) 0);
+        }
+    }
 }
