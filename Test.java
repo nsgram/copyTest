a35763955c52c8291ec5114e86330c31a965df57
nsgram <68147300+nsgram@users.noreply.github.com>
2025-03-13 19:41:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,38 +1,84 @@
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.Map;
-
-public String uploadToAdobe(MultipartFile file, String authorizationKey) throws Exception {
-    String url = "https://api.na2.documents.adobe.com/api/rest/v6/transientDocuments";
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("Authorization", "Bearer " + authorizationKey);
-    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-    // Convert file to ByteArrayResource
-    ByteArrayResource fileResource = new ByteArrayResource(file.getBytes()) {
-        @Override
-        public String getFilename() {
-            return file.getOriginalFilename(); // Important: Adobe API expects a proper filename
+{
+    "fileInfos": [
+        {
+            "document": null,
+            "label": null,
+            "libraryDocumentId": null,
+            "transientDocumentId": "CBSCTBABDUAAABACAABAAeZ4mh3uIRO6nLuRJLKl5fpVy6oLf_OPbZvBd4mTYBXzIT7rNs5I-VaNYUzEBZ39mKO4qlZziB7XF3wLWEGq5l2OmlS8BWYfw4LqJuULDhlcgHtvVQOSot2OHxVGwRcfAEJmcqCZ4Qx-N3E-mDPW5T3lm6QkjHpfxhuy6Ix5xFkYPtZoz_mZHfoBB9kKX_d4R98mP9xrTk1-XYDyuta0mZC-d8TxldOyOJKbvbHj9ZR1AnS-phmKBGRS0YdbNs5AUzB57BPYXtZgSvn9DivZTwTytpyeIjriJnaiAV8DMBIJymWZLoprJK2a5RZzatj3G_jeyeCO3oUZ2i8nI8TqFo2KSyLN6FnDhzihKwOKiHfw*",
+            "urlFileInfo": null
         }
-    };
-
-    // Create multipart body
-    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-    body.add("File-Name", file.getOriginalFilename());
-    body.add("File", fileResource);
-
-    HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Map.class);
-
-    if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
-        return (String) response.getBody().get("transientDocumentId");
-    } else {
-        throw new RuntimeException("Failed to upload file: " + response.getBody());
-    }
+    ],
+    "name": " - Application for insurance",
+    "participantSetsInfo": [
+        {
+            "memberInfos": [
+                {
+                    "email":  "lavater@aetna.com",
+                    "securityOption": {
+                        "phoneInfo": {
+                            "countryCode": "+1",
+                            "countryIsoCode": null,
+                            "phone": "3331212121"
+                        }
+                    }
+                }
+            ],
+            "order": 1,
+            "role": "SIGNER",
+            "label": null,
+            "name": null,
+            "privateMessage": null,
+            "visiblePages": null
+        },
+        {
+            "memberInfos": [
+                {
+                    "email": "lavater@aetna.com",
+                    "securityOption": {
+                        "phoneInfo": {
+                            "countryCode": "+1",
+                            "countryIsoCode": null,
+                            "phone": "3331212121"
+                        }
+                    }
+                }
+            ],
+            "order": 2,
+            "role": "SIGNER",
+            "label": null,
+            "name": null,
+            "privateMessage": null,
+            "visiblePages": null
+        }
+    ],
+    "signatureType": "ESIGN",
+    "state": "IN_PROCESS",
+    "ccs": [
+        {
+            "email": "lavater@aetna.com",
+            "label": null,
+            "visiblePages": null
+        }
+    ],
+    "createdDate": null,
+    "deviceInfo": null,
+    "documentVisibilityEnabled": null,
+    "emailOption": null,
+    "expirationTime": null,
+    "externalId": null,
+    "firstReminderDelay": null,
+    "formFieldLayerTemplates": null,
+    "groupId": null,
+   "id": null,
+    "isDocumentRetentionApplied": null,
+    "locale": null,
+    "mergeFieldInfo": null,
+    "message": "\nThank you for applying for insurance with Accendo Insurance Company. This email contains instructions for Applicant, Owner (if applicable) to apply electronic signature on the application and related documents.\n\nInstructions for Applicant(s)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the 'Review and Sign' button above. Your unique password is the last 4 digits of your SSN.\n\nInstructions for Owner(if applicable)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the 'Review and Sign' button above. Your unique password is the last 4 digits of your SSN.\n\nAfter all email signatures are completed, application will be automatically sent to our home office for processing",
+    "postSignOption": null,
+    "reminderFrequency": null,
+    "securityOption": null,
+    "senderEmail": null,
+    "status": null,
+    "vaultingInfo": null,
+    "workflowId": null
 }
