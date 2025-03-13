4932699db6f7a481766888e92450eb9495ef3d6e
nsgram <68147300+nsgram@users.noreply.github.com>
2025-03-13 23:31:59 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,94 +1,116 @@
+Java Code
+
+
+@PostMapping(value = "/esign/create-proposal", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<String> createAgreement(@RequestPart("file") MultipartFile file,
+			@RequestPart("data") AdobeSignRequest adobeSignRequest) {
+
+		return null;
+
+	}
+
+
+Request
 curl -X 'POST' \
-  'https://api.na2.documents.adobe.com/api/rest/v6/agreements' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer 3AAABLblqZhAGDFfvYVDORbzoUnKCkbG7PeSsy3wlPkAWvORfmQbE5OQZxpEzbJAB7BRA3_3qYeUqkWyvNkITfabRokoDZElC' \
-  -d '{
-    "fileInfos": [
+  'http://localhost:9080/asgwy-api/v1/quote/esign/create-proposal' \
+  -H 'accept: */*' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'file=@document.pdf;type=application/pdf' \
+  -F 'data={
+  "fileInfos": [
+    {
+      "document": null,
+      "label": null,
+      "libraryDocumentId": null,
+      "transientDocumentId": "CBSCTBABDUAAABACAABAAYCRsRIeWS24wk7PjadWLKZtbj7KxmJSgC9MiIXHdjLF3kRRzR2epvPdsslBowlyTwUEVs7lmaIxynTfiS-usNfT6hEa1ctLgQbNl4ku6YOW36IF4IIvAl3YGDPXh3BdSEOqEcQYs3z5Q2HsMHG4YqsQDez99P0PSKIf8X-qxdD02a7O8ihbFzlPEMNw6JuKKw3gb5ED01vZ66EyUXOhJPIMnTi6trg6LnpmjbII6yX3dhClQtICIo2OpZBmaU5iuAQSezfKYNxDkeqHzr54qMNCtu1AFAbirAg-cMPLyepDoMC1NF2OCyc0xFromxQA5Bi0bn2366kge5ZTxts49KYJouYsUmWsQtN5EkCb6AQ0*",
+      "urlFileInfo": null
+    }
+  ],
+  "name": " - Application for insurance",
+  "participantSetsInfo": [
+    {
+      "memberInfos": [
         {
-            "document": null,
-            "label": null,
-            "libraryDocumentId": null,
-            "transientDocumentId": "CBSCTBABDUAAABACAABAAeZ4mh3uIRO6nLuRJLKl5fpVy6oLf_OPbZvBd4mTYBXzIT7rNs5I-VaNYUzEBZ39mKO4qlZziB7XF3wLWEGq5l2OmlS8BWYfw4LqJuULDhlcgHtvVQOSot2OHxVGwRcfAEJmcqCZ4Qx-N3E-mDPW5T3lm6QkjHpfxhuy6Ix5xFkYPtZoz_mZHfoBB9kKX_d4R98mP9xrTk1-XYDyuta0mZC-d8TxldOyOJKbvbHj9ZR1AnS-phmKBGRS0YdbNs5AUzB57BPYXtZgSvn9DivZTwTytpyeIjriJnaiAV8DMBIJymWZLoprJK2a5RZzatj3G_jeyeCO3oUZ2i8nI8TqFo2KSyLN6FnDhzihKwOKiHfw*",
-            "urlFileInfo": null
+          "email": "lavater@aetna.com",
+          "securityOption": {
+            "phoneInfo": {
+              "countryCode": "+1",
+              "countryIsoCode": "00",
+              "phone": "3331212121"
+            }
+          }
         }
-    ],
-    "name": " - Application for insurance",
-    "participantSetsInfo": [
+      ],
+      "order": 1,
+      "role": "SIGNER",
+      "label": null,
+      "name": null,
+      "privateMessage": null,
+      "visiblePages": [
+        null
+      ]
+    },
+	{
+      "memberInfos": [
         {
-            "memberInfos": [
-                {
-                    "email":  "lavater@aetna.com",
-                    "securityOption": {
-                        "phoneInfo": {
-                            "countryCode": "+1",
-                            "countryIsoCode": null,
-                            "phone": "3331212121"
-                        }
-                    }
-                }
-            ],
-            "order": 1,
-            "role": "SIGNER",
-            "label": null,
-            "name": null,
-            "privateMessage": null,
-            "visiblePages": null
-        },
-        {
-            "memberInfos": [
-                {
-                    "email": "lavater@aetna.com",
-                    "securityOption": {
-                        "phoneInfo": {
-                            "countryCode": "+1",
-                            "countryIsoCode": null,
-                            "phone": "3331212121"
-                        }
-                    }
-                }
-            ],
-            "order": 2,
-            "role": "SIGNER",
-            "label": null,
-            "name": null,
-            "privateMessage": null,
-            "visiblePages": null
+          "email": "lavater@aetna.com",
+          "securityOption": {
+            "phoneInfo": {
+              "countryCode": "+1",
+              "countryIsoCode": "22",
+              "phone": "222222222"
+            }
+          }
         }
-    ],
-    "signatureType": "ESIGN",
-    "state": "IN_PROCESS",
-    "ccs": [
-        {
-            "email": "lavater@aetna.com",
-            "label": null,
-            "visiblePages": null
-        }
-    ],
-    "createdDate": null,
-    "deviceInfo": null,
-    "documentVisibilityEnabled": null,
-    "emailOption": null,
-    "expirationTime": null,
-    "externalId": null,
-    "firstReminderDelay": null,
-    "formFieldLayerTemplates": null,
-    "groupId": null,
-   "id": null,
-    "isDocumentRetentionApplied": null,
-    "locale": null,
-    "mergeFieldInfo": null,
-    "message": "\nThank you for applying for insurance with Accendo Insurance Company. This email contains instructions for Applicant, Owner (if applicable) to apply electronic signature on the application and related documents.\n\nInstructions for Applicant(s)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the '\''Review and Sign'\'' button above. Your unique password is the last 4 digits of your SSN.\n\nInstructions for Owner(if applicable)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the '\''Review and Sign'\'' button above. Your unique password is the last 4 digits of your SSN.\n\nAfter all email signatures are completed, application will be automatically sent to our home office for processing",
-    "postSignOption": null,
-    "reminderFrequency": null,
-    "securityOption": null,
-    "senderEmail": null,
-    "status": null,
-    "vaultingInfo": null,
-    "workflowId": null
-}
-'
-Response
+      ],
+      "order": 2,
+      "role": "SIGNER",
+      "label": null,
+      "name": null,
+      "privateMessage": null,
+      "visiblePages": [
+        null
+      ]
+    }
+  ],
+  "signatureType": "ESIGN",
+  "state": "IN_PROCESS",
+  "ccs": [
+    {
+      "email": "lavater@aetna.com",
+      "label": null,
+      "visiblePages": [
+        null
+      ]
+    }
+  ],
+  "createdDate": null,
+  "deviceInfo": null,
+  "documentVisibilityEnabled": null,
+  "emailOption": null,
+  "expirationTime": null,
+  "externalId": null,
+  "firstReminderDelay": null,
+  "formFieldLayerTemplates": null,
+  "groupId": null,
+  "id": null,
+  "isDocumentRetentionApplied": null,
+  "locale": null,
+  "mergeFieldInfo": null,
+  "message": "\nThank you for applying for insurance with Accendo Insurance Company. This email contains instructions for Applicant, Owner (if applicable) to apply electronic signature on the application and related documents.\n\nInstructions for Applicant(s)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the '\''Review and Sign'\'' button above. Your unique password is the last 4 digits of your SSN.\n\nInstructions for Owner(if applicable)\n\nBefore you sign, please review the application for accuracy and completeness. If you have any questions, contact your agent. Please do not reply to this e-mail. To get started, please click on the '\''Review and Sign'\'' button above. Your unique password is the last 4 digits of your SSN.\n\nAfter all email signatures are completed, application will be automatically sent to our home office for processing",
+  "postSignOption": null,
+  "reminderFrequency": null,
+  "securityOption": null,
+  "senderEmail": null,
+  "status": null,
+  "vaultingInfo": null,
+  "workflowId": null
+}'
+response
 {
-  "id": "CBJCHBCAABAA99jurZPdI2CVydkQuFrF8dCz5F8q3rXQ"
+  "type": "about:blank",
+  "title": "Unsupported Media Type",
+  "status": 415,
+  "detail": "Content-Type 'application/octet-stream' is not supported.",
+  "instance": "/asgwy-api/v1/quote/esign/create-proposal"
 }
