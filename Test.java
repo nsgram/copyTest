70a55d79bf90266010d161aac1abbf90a6d779f2
nsgram <68147300+nsgram@users.noreply.github.com>
2025-04-14 11:33:03 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,3 +1,47 @@
-private byte[] getDummyPdfBytes() {
-    return "%PDF-1.4\n%âãÏÓ\n1 0 obj\n<< /Type /Catalog >>\nendobj\nxref\n0 1\n0000000000 65535 f \ntrailer\n<< /Root 1 0 R >>\nstartxref\n9\n%%EOF".getBytes(StandardCharsets.UTF_8);
+package com.example.pdfsignature.controller;
+
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.*;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+
+@RestController
+@RequestMapping("/pdf")
+public class PdfSignatureController {
+
+    @GetMapping("/generate-pdf-with-signature")
+    public ResponseEntity<byte[]> generatePdfWithSignature() throws IOException {
+        InputStream templatePdf = new ClassPathResource("templates/sample.pdf").getInputStream();
+
+        byte[] updatedPdf = addSignatureField(templatePdf);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDisposition(ContentDisposition.attachment().filename("fillable-with-signature.pdf").build());
+
+        return new ResponseEntity<>(updatedPdf, headers, HttpStatus.OK);
+    }
+
+    private byte[] addSignatureField(InputStream pdfInputStream) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        PdfReader reader = new PdfReader(pdfInputStream);
+        PdfStamper stamper = new PdfStamper(reader, outputStream);
+
+        PdfFormField signatureField = PdfFormField.createSignature(stamper.getWriter());
+        signatureField.setFieldName("signature");
+        signatureField.setWidget(new Rectangle(100, 100, 300, 150), PdfAnnotation.HIGHLIGHT_INVERT);
+        signatureField.setFlags(PdfAnnotation.FLAGS_PRINT);
+        signatureField.setPage(1);
+
+        stamper.addAnnotation(signatureField, 1);
+        stamper.close();
+        reader.close();
+
+        return outputStream.toByteArray();
+    }
 }
